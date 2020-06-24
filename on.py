ec44f10723918962b7e8259e2526f7903a7611e0
Emery <abemery@clemson.edu>
2020-06-24 01:19:18 -0400

    turn on

--- on.py
+++ on.py
@@ -8,11 +8,7 @@ off = [0, 0, 0]
 grid = sense.get_pixels()
 mid = int(len(grid)/2)
 
-if on in grid:
-    for i in range(64):
-        grid[i] = off
-else:
-    for i in range(mid-5,mid+5):
-        grid[i] = on
+for i in range(64):
+    grid[i] = on
 
 sense.set_pixels(grid)
