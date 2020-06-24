9d2d7d328c3f9240a926b8f0da9caf0a888abc56
Emery <abemery@clemson.edu>
2020-06-24 00:36:58 -0400

    probably not supposed to do it this way

--- toggleLight.py
+++ toggleLight.py
@@ -6,7 +6,8 @@ on = [255, 255, 255]
 off = [0, 0, 0]
 
 grid = [off for i in range(64)]
-grid[int(len(grid)/2)] = on
+mid = int(len(grid)/2)
+grid[(len - 5):(mid + 5)] = on
 # print(grid)
 
 sense.set_pixels(grid)
